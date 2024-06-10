5ec34cb9c05b8462d4509118885bff0848ab5d64
Dmitry Sinitsin <sinicyn.mitrij1@yandex.ru>
2024-06-10 22:17:34 +0300

    Title15 N3

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,34 @@
 #include <string>
 #include <cctype>
 #include <algorithm>
+#include <set>
+#include <vector>
+#include <iterator>
 
-bool isPalindrome(const std::string& st);
+int reduce(long ar[], int n);
 
 int main()
 {
-    std::string str;
-    bool fail = false;
-    std::cout << "Enter the string! Or \"quit\" to quit\n";
-    getline(std::cin, str);
-    while (str != "quit")
+    std::vector<long> long_array;
+    long num;
+    std::ostream_iterator<long> out(std::cout, " ");
+    std::cout << "Enter the numbers! Or \"quit\" to quit\n";
+    while (std::cin >> num)
     {
-        std::string new_str;
-        for (char it : str)
-            if (isalpha(it))
-                new_str += it;
-        transform(new_str.begin(), new_str.end(), new_str.begin(), tolower);
-        if (isPalindrome(new_str))
-            std::cout << "String \"" << new_str << "\" is palidrome!\n";
-        else
-            std::cout << "String \"" << new_str << "\" isn't palidrome!\n";
-        std::cout << "Enter the string! Or \"quit\" to quit\n";
-        getline(std::cin, str);
+        long_array.push_back(num);
     }
-    std::cout << "Bye!" << '\n';
+    std::cout << "Entered vector was:\n";
+    std::copy(long_array.begin(), long_array.end(), out);
+    std::cout << "\nThe conversion vector has "<< reduce(&long_array[0], long_array.size()) << '\n';
     return 0;
 }
 
-bool isPalindrome(const std::string& st)
+int reduce(long ar[], int n)
 {
-    size_t sz = st.size();
-    int half = (sz / 2) + 1;
-
-    for (int i = 0; i < half ; ++i)
-    {
-        if (st.at(i) != st.at(sz-1-i))
-            return false;
-    }
-    return true;
+    std::set<long> s;
+    std::copy(ar, ar + n, std::insert_iterator<std::set<long>>(s, s.begin()));
+    std::cout << "\n";
+    std::copy(s.begin(), s.end(), std::ostream_iterator<long> (std::cout, " "));
+    std::cout << "\n";
+    return s.size();
 }
