c1cc6b5a3382b6e73aa2dbc9b6f8e691450c6087
Dmitry Sinitsin <sinicyn.mitrij1@yandex.ru>
2024-06-12 21:48:06 +0300

    Title15 N6

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,42 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <queue>
-#include "../headers/queue.h"
+#include <vector>
+#include <set>
+#include <iterator>
 
 const int MIN_PER_HOUR = 60;
+
+std::vector<int> Lotto(int total, int setted);
 
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
-
-    std::cout << "Bank of Heather\n";
-    std::cout << "Enter maximum size of queue: ";
-    int max_line;
-    std::cin >> max_line;
-
-    std::queue<Item> line;
-    std::cout << "Enter the number of simulation hours: ";
-    int hours;
-    std::cin >> hours;
-    long cyclelimit = MIN_PER_HOUR * hours;
-    std::cout << "Enter the average number of customers per hour: ";
-    double cust_per_hour;
-    std::cin >> cust_per_hour;
-    double min_per_cust = MIN_PER_HOUR / cust_per_hour;
-
-    Item cust;
-    long turnaways = 0; //ушедших клиентов от полной очереди
-    long customers = 0; //присоединений к очереди
-    long served = 0;    //обслуженных клиентов
-    long sum_line = 0;  //совокупная длина очереди
-    int wait_time = 0;  //период времени когда автоотвечтик свободен
-    long line_wait = 0; //общее время в линии
-
-    for (int cycle = 0; cycle < cyclelimit; cycle++)
-    {
-        if (new_customer(min_per_cust))
-        {
-            if (line.size() == max_line)
-                turnaways++;
-            else
-            {
-                customers++;
-                cust.set(cycle);
-                line.push(cust);
-            }
-        }
-        if (wait_time <= 0 && !line.empty())
-        {
-           cust = line.front();
-           line.pop();
-           wait_time = cust.ptime();
-           line_wait += cycle - cust.when();
-           served++;
-        }
-        if(wait_time > 0)
-            wait_time--;
-        sum_line += line.size();
-    }
-
-    if(customers > 0)
-    {
-        std::cout << "\ncustomers accepted: " << customers << std::endl;
-        std::cout << "customers served: " << served << std::endl;
-        std::cout << "customers turnaways: " << turnaways << std::endl;
-        std::cout << "average queue size: ";
-        std::cout.precision(2);
-        std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
-        std::cout.setf(std::ios_base::showpoint);
-        std::cout << (double)sum_line / cyclelimit << std::endl;
-        std::cout << "average line wait time: ";
-        std::cout << (double)line_wait / served << std::endl;
-    }
-    else
-        std::cout << "No customers!" << std::endl;
-
+    int total = 0;
+    int setted = 0;
+    std::ostream_iterator<int> out(std::cout, " ");
+    std::cout << "Enter total count of numbers:";
+    std::cin >> total;
+    std::cout << "Enter count of setted numbers:";
+    std::cin >> setted;
+    std::cin.clear();
+    std::cin.get();
+    std::cout << "Got vector:\n";
+    std::vector<int> vec = Lotto(total, setted);
+    std::copy(vec.begin(), vec.end(), out);
+    std::cout << "\n";
     return 0;
+}
+
+std::vector<int> Lotto(int total, int setted)
+{
+    if(setted > total)
+        setted = total;
+    std::set<int> temp;
+    std::vector<int> vec(setted);
+    std::insert_iterator<std::set<int>> it(temp, temp.begin());
+    for(; temp.size() < setted; ++it)
+        it = rand() % total + 1;
+    std::copy(temp.begin(), temp.end(), vec.begin());
+    return vec;
 }
