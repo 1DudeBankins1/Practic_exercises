cc58e58f08eebab3e163888174f58abdc14d0208
Dmitry Sinitsin <sinicyn.mitrij1@yandex.ru>
2024-07-07 14:25:50 +0300

    Title16 N5

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <fstream>
+#include <set>
+#include <iterator>
+#include <algorithm>
 
 inline void eatLine(){
     while (std::cin.get() != '\n') continue;
@@ -8,67 +11,79 @@ inline void eatLine(){
 
 int main(int argc, char* argv[])
 {
+    std::ostream_iterator<std::string> out(std::cout, " ");
+    std::cout << "Matt, enter your filename:\n";
+
     char fileName[40];
-    std::cout << "Choose files for concatenating:\n";
     std::cin.get(fileName, 40, '\n');
     eatLine();
-    std::ifstream cFile1;
-    cFile1.open(fileName, std::ios_base::binary | std::ios_base::in);
-    if (!cFile1.is_open()){
+    std::ifstream mattsFile;
+    mattsFile.open(fileName, std::ios_base::in);
+    if (!mattsFile.is_open()){
         std::cerr << "The file isn't found\n";
         exit(1);
     }
+
+    std::set<std::string> matts;
+    std::string temp;
+    while(getline(mattsFile, temp)){
+        matts.insert(temp);
+    }
+
+    std::cout << "Patt, enter your filename:\n";
     std::cin.get(fileName, 40, '\n');
     eatLine();
-    std::ifstream cFile2;
-    cFile2.open(fileName, std::ios_base::binary | std::ios_base::in);
-    if (!cFile2.is_open()){
+    std::ifstream pattsFile;
+    pattsFile.open(fileName);
+    if (!pattsFile.is_open()){
         std::cerr << "The file isn't found\n";
         exit(1);
+    }
+    std::set<std::string> patts;
+    while(getline(pattsFile, temp)){
+        patts.insert(temp);
     }
 
     std::cout << "Choose the file for destination:\n";
     std::cin.get(fileName, 40, '\n');
     eatLine();
     std::ofstream destFile;
-    destFile.open(fileName, std::ios_base::binary | std::ios_base::out | std::ios_base::trunc);
+    destFile.open(fileName, std::ios_base::out | std::ios_base::trunc);
     if (!destFile.is_open()){
         std::cerr << "The file isn't found\n";
         exit(1);
     }
 
-    char ch;
-    while(!cFile1.eof() || !cFile1.eof()){
-        if(!cFile1.eof()){
-            cFile1.get(ch);
-            while(ch != '\n'){
-                destFile << ch;
-                cFile1.get(ch);
-            }
-        }
-        destFile << ' ';
-        if(!cFile2.eof()){
-            cFile2.get(ch);
-            while(ch != '\n'){
-                destFile << ch;
-                cFile2.get(ch);
-            }
-        }
-        destFile << std::endl;
+    std::set<std::string> united;
+    std::insert_iterator<std::set<std::string>>it(united, united.begin());
+    std::set_union(matts.begin(), matts.end(), patts.begin(), patts.end(), it);
+
+    for (auto itt : united){
+        destFile << itt << std::endl;
     }
 
-    cFile1.clear();
-    cFile1.close();
-    cFile2.clear();
-    cFile2.close();
+    std::cout << "\nMatt's list: ";
+    std::copy(matts.begin(), matts.end(), out);
+
+    std::cout << "\nPatt's list: ";
+    std::copy(patts.begin(), patts.end(), out);
+
+    std::cout << "\nUnited list: ";
+    std::copy(united.begin(), united.end(), out);
+
+    mattsFile.clear();
+    mattsFile.close();
+    pattsFile.clear();
+    pattsFile.close();
     destFile.clear();
     destFile.close();
 
     std::ifstream inFile;
     inFile.open(fileName);
-    std::cout << "The file " << fileName << " contains:\n";
-    while((ch = inFile.get()) != EOF){
-        std::cout << ch;
+    std::cout << '\n';
+    std::cout << "The file " << fileName << " contains:";
+    while(getline(inFile, temp)){
+        std::cout << temp << '\n';
     }
     std::cout << std::endl;
     inFile.clear();
